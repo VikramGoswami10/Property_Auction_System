ff6f1893fe82c0e1b01ea79ef95fcf2c4e98062c
vikram Goswami <vikramgoswami4k@gmail.com>
2024-12-16 23:11:08 +0530

    Resolved merge conflicts and fixed component imports

--- frontend/src/Components/hero/CategorySlider.js
+++ frontend/src/Components/hero/CategorySlider.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import { categorylists } from "../../Routes";
-import { Container, Heading } from "../../Routes";
-import "./styles/CategorySlider.css";
-
-const CategorySlider = () => {
-  return (
-    <section className="category-slider pb-16">
-      <Container>
-        <Heading title="Browse the categories" subtitle="Most viewed and all-time top-selling categories" />
-        <div className="property-container">
-          <div className="property-types">
-            {categorylists.map((property, index) => (
-              <div key={index} className="property-card">
-                <img src={property.img} alt={property.title} className="property-image" />
-                <div className="overlay">
-                  <span className="property-title">{property.title}</span>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </Container>
-    </section>
-  );
-};
-
-export default CategorySlider;
-=======
 import { categorylists } from "../../Utils/Data";
 import { Container,Heading } from "../../Routes"
 import "./styles/CategorySlider.css"
@@ -54,5 +25,5 @@ const CategorySlider = () => {
 };
 
 
-export { CategorySlider };
->>>>>>> home/adding-homeSearch-category
+export default CategorySlider;
+
